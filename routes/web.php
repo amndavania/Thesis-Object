05399653b596062061eb2c6702089f08a8b5fef9
Ahmad Zidni Zainul Ikhsan <58660486+zidny-z@users.noreply.github.com>
2023-05-06 22:53:43 +0700

    email
    -language didn't work

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
