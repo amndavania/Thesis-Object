029f006dd00dba0f13cc50113df361307020418c
Izaz Dhiyaulhaq <id.izaz123@gmail.com>
2023-07-03 23:42:47 +0700

    setting dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -75,36 +75,36 @@
                 <div class="card">
                   <div class="card-header border-0">
                     <div class="d-flex justify-content-between">
-                      <h3 class="card-title">Sales</h3>
-                      <a href="javascript:void(0);">View Report</a>
+                      <h3 class="card-title">Keuangan</h3>
+                      <a href="javascript:void(0);">Lihat Laporan</a>
                     </div>
                   </div>
                   <div class="card-body">
                     <div class="d-flex">
                       <p class="d-flex flex-column">
                         <span class="text-bold text-lg">$18,230.00</span>
-                        <span>Sales Over Time</span>
+                        <span>Perkembangan keuangan</span>
                       </p>
                       <p class="ml-auto d-flex flex-column text-right">
                         <span class="text-success">
                           <i class="fas fa-arrow-up"></i> 33.1%
                         </span>
-                        <span class="text-muted">Since last month</span>
+                        <span class="text-muted">Sejak bulan lalu</span>
                       </p>
                     </div>
                     <!-- /.d-flex -->
 
                     <div class="position-relative mb-4">
-                      <canvas id="sales-chart" height="200"></canvas>
+                      <canvas id="balance-chart" height="200"></canvas>
                     </div>
 
                     <div class="d-flex flex-row justify-content-end">
                       <span class="mr-2">
-                        <i class="fas fa-square text-primary"></i> This year
+                        <i class="fas fa-square text-primary"></i> Tahun ini
                       </span>
 
                       <span>
-                        <i class="fas fa-square text-gray"></i> Last year
+                        <i class="fas fa-square text-gray"></i> Tahun lalu
                       </span>
                     </div>
                   </div>
@@ -152,7 +152,7 @@
 
                 <div class="card">
                   <div class="card-header border-0">
-                    <h3 class="card-title">Pembayaran Mahasiswa</h3>
+                    <h3 class="card-title">Akun transaksi yang sering digunakan</h3>
                     <div class="card-tools">
                       <a href="#" class="btn btn-tool btn-sm">
                         <i class="fas fa-download"></i>
@@ -259,45 +259,73 @@
               <!-- /.col-md-6 -->
               <div class="col-lg-6">
                 <div class="card">
-                  <div class="card-header border-0">
-                    <div class="d-flex justify-content-between">
-                      <h3 class="card-title">Sales</h3>
-                      <a href="javascript:void(0);">View Report</a>
+                    <div class="card-header">
+                      <h3 class="card-title">Pembayaran UKT Mahasiswa</h3>
+      
+                      <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                          <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                          <i class="fas fa-times"></i>
+                        </button>
+                      </div>
                     </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-md-8">
+                          <div class="chart-responsive">
+                            <canvas id="pieChart" height="150"></canvas>
+                          </div>
+                          <!-- ./chart-responsive -->
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-md-4">
+                          <ul class="chart-legend clearfix">
+                            <li><i class="far fa-circle text-success"></i> Lunas</li>
+                            <li><i class="far fa-circle text-warning"></i> Belum Lunas</li>
+                            <li><i class="far fa-circle text-danger"></i> Belum Bayar</li>
+                          </ul>
+                        </div>
+                        <!-- /.col -->
+                      </div>
+                      <!-- /.row -->
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer p-0">
+                      <ul class="nav nav-pills flex-column">
+                        <li class="nav-item">
+                          <a href="#" class="nav-link">
+                            United States of America
+                            <span class="float-right text-danger">
+                              <i class="fas fa-arrow-down text-sm"></i>
+                              12%</span>
+                          </a>
+                        </li>
+                        <li class="nav-item">
+                          <a href="#" class="nav-link">
+                            India
+                            <span class="float-right text-success">
+                              <i class="fas fa-arrow-up text-sm"></i> 4%
+                            </span>
+                          </a>
+                        </li>
+                        <li class="nav-item">
+                          <a href="#" class="nav-link">
+                            China
+                            <span class="float-right text-warning">
+                              <i class="fas fa-arrow-left text-sm"></i> 0%
+                            </span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                    <!-- /.footer -->
                   </div>
-                  <div class="card-body">
-                    <div class="d-flex">
-                      <p class="d-flex flex-column">
-                        <span class="text-bold text-lg">$18,230.00</span>
-                        <span>Sales Over Time</span>
-                      </p>
-                      <p class="ml-auto d-flex flex-column text-right">
-                        <span class="text-success">
-                          <i class="fas fa-arrow-up"></i> 33.1%
-                        </span>
-                        <span class="text-muted">Since last month</span>
-                      </p>
-                    </div>
-                    <!-- /.d-flex -->
-
-                    <div class="position-relative mb-4">
-                      <canvas id="sales-chart" height="200"></canvas>
-                    </div>
-
-                    <div class="d-flex flex-row justify-content-end">
-                      <span class="mr-2">
-                        <i class="fas fa-square text-primary"></i> This year
-                      </span>
-
-                      <span>
-                        <i class="fas fa-square text-gray"></i> Last year
-                      </span>
-                    </div>
-                  </div>
-                </div>
                 <!-- /.card -->
 
-                <div class="card">
+                {{-- <div class="card">
                   <div class="card-header border-0">
                     <h3 class="card-title">Transaksi Terbaru</h3>
                     <div class="card-tools">
@@ -347,7 +375,7 @@
                     </div>
                     <!-- /.d-flex -->
                   </div>
-                </div>
+                </div> --}}
               </div>
               <!-- /.col-md-6 -->
             </div>
--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -20,14 +20,6 @@
 
 </script>
 
-{{-- <script>
-    $("#datepicker").datepicker( {
-        format: "mm-yyyy",
-        startView: "months",
-        minViewMode: "months"
-    });
-</script> --}}
-
 <script>
     function handleFilterChange() {
         var filter = document.getElementById('filter').value;
@@ -113,5 +105,153 @@
     $(document).ready(function() {
       $('#accounting_group_id').select2();
     });
+</script>
+
+<script>
+    $(function () {
+        'use strict'
+
+        var ticksStyle = {
+            fontColor: '#495057',
+            fontStyle: 'bold'
+        }
+
+        var mode = 'index'
+        var intersect = true
+
+        var $salesChart = $('#balance-chart')
+        // eslint-disable-next-line no-unused-vars
+        var salesChart = new Chart($salesChart, {
+            type: 'bar',
+            data: {
+            labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+            datasets: [
+                {
+                backgroundColor: '#007bff',
+                borderColor: '#007bff',
+                data: [100000000, 200000000, 300000000, 250000000, 270000000, 250000000, 300000000, 100000000, 200000000, 300000000, 250000000, 270000000]
+                },
+                {
+                backgroundColor: '#ced4da',
+                borderColor: '#ced4da',
+                data: [70000000, 170000000, 270000000, 200000000, 180000000, 150000000, 200000000, 70000000, 170000000, 270000000, 200000000, 180000000]
+                }
+            ]
+            },
+            options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                mode: mode,
+                intersect: intersect
+            },
+            hover: {
+                mode: mode,
+                intersect: intersect
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                // display: false,
+                gridLines: {
+                    display: true,
+                    lineWidth: '4px',
+                    color: 'rgba(0, 0, 0, .2)',
+                    zeroLineColor: 'transparent'
+                },
+                ticks: $.extend({
+                    beginAtZero: true,
+
+                    // Include a dollar sign in the ticks
+                    callback: function (value) {
+                    if (value >= 1000000) {
+                        value /= 1000000
+                        value += 'jt'
+                    }
+
+                    // return 'Rp' + value
+                    return value
+                    }
+                }, ticksStyle)
+                }],
+                xAxes: [{
+                display: true,
+                gridLines: {
+                    display: false
+                },
+                ticks: ticksStyle
+                }]
+            }
+            }
+        })
+
+        var $visitorsChart = $('#visitors-chart')
+        // eslint-disable-next-line no-unused-vars
+        var visitorsChart = new Chart($visitorsChart, {
+            data: {
+            labels: ['18th', '20th', '22nd', '24th', '26th', '28th', '30th'],
+            datasets: [{
+                type: 'line',
+                data: [100, 120, 170, 167, 180, 177, 160],
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                pointBorderColor: '#007bff',
+                pointBackgroundColor: '#007bff',
+                fill: false
+                // pointHoverBackgroundColor: '#007bff',
+                // pointHoverBorderColor    : '#007bff'
+            },
+            {
+                type: 'line',
+                data: [60, 80, 70, 67, 80, 77, 100],
+                backgroundColor: 'tansparent',
+                borderColor: '#ced4da',
+                pointBorderColor: '#ced4da',
+                pointBackgroundColor: '#ced4da',
+                fill: false
+                // pointHoverBackgroundColor: '#ced4da',
+                // pointHoverBorderColor    : '#ced4da'
+            }]
+            },
+            options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                mode: mode,
+                intersect: intersect
+            },
+            hover: {
+                mode: mode,
+                intersect: intersect
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                // display: false,
+                gridLines: {
+                    display: true,
+                    lineWidth: '4px',
+                    color: 'rgba(0, 0, 0, .2)',
+                    zeroLineColor: 'transparent'
+                },
+                ticks: $.extend({
+                    beginAtZero: true,
+                    suggestedMax: 200
+                }, ticksStyle)
+                }],
+                xAxes: [{
+                display: true,
+                gridLines: {
+                    display: false
+                },
+                ticks: ticksStyle
+                }]
+            }
+            }
+        })
+        })
+
 </script>
 
