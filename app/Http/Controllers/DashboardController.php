be7c25f3135f5fef550454e385c7aa2a1d1b098d
Izaz Dhiyaulhaq <id.izaz123@gmail.com>
2023-07-05 17:34:39 +0700

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -23,6 +23,11 @@ class DashboardController extends Controller
         $date = date('Y-m');
         $filter = 'month';
 
+        $financeThisYear = $this->setTrendKeuangan('ThisYear');
+        $financeLastYear = $this->setTrendKeuangan('LastYear');
+
+        // dd($financeThisYear);
+
         $cashflow_group = [
             'arusKasMasuk' => 11,
             'arusKasKeluar' => 12,
@@ -40,24 +45,83 @@ class DashboardController extends Controller
             'pendapatanPengeluaranLain' => 5,
         ];
 
-        $cashflow = $cashflowController->setResults($filter, $date, $cashflow_group);
-        $saldoAwal = $cashflowController->getSaldoAwal($filter, $date);
-        $saldoAkhir = array_sum(array_map(function ($group) {
-            return array_sum(array_column($group, 'saldo'));
-        }, $cashflow)) + $saldoAwal;
-
-        $labarugi = $labarugiController->setResults($filter, $date, $labarugi_group);
-        $saldoLabaRugi = array_sum(array_map(function ($group) {
-            return array_sum(array_column($group, 'saldo'));
-        }, $labarugi));
+        $saldoAkhir = $this->setSaldoAkhir($cashflowController, $filter, $date, $cashflow_group);
+        $saldoLabaRugi = $this->setLabaRugi($labarugiController, $filter, $date, $labarugi_group);
 
         $students = Student::all();
+        $transactions = Transaction::latest()->take(5)->get();
 
         return view('dashboard')->with([
             'saldo' => $saldoAkhir,
             'labarugi' => $saldoLabaRugi,
             'students' => $students->count(),
+            'trendKeuangan' => [$financeThisYear, $financeLastYear],
+            'transactions' => $transactions
         ]);
     }
+
+    public function setSaldoAkhir($cashflowController, $filter, $date, $cashflow_group) {
+        $saldo = [];
+        $cashflow = $cashflowController->setResults($filter, $date, $cashflow_group);
+        foreach ($cashflow as $key => $value) {
+            $saldo_item = 0;
+            foreach ($value as $item) {
+                if (isset($item["saldo"])) {
+                    $saldo_item += $item["saldo"];
+                }
+            }
+            $saldo[$key] = $saldo_item;
+        }
+        $arusKasMasuk = $saldo["arusKasMasuk"];
+        $arusKasKeluar = $saldo["arusKasKeluar"];
+        $penjualanAset = $saldo["penjualanAset"];
+        $pembelianAset = $saldo["pembelianAset"];
+        $penambahanDana = $saldo["penambahanDana"];
+        $penguranganDana = $saldo["penguranganDana"];
+
+        $selisihKas = $arusKasMasuk - $arusKasKeluar;
+        $selisihAset = $penjualanAset - $pembelianAset;
+        $selisihDana = $penambahanDana - $penguranganDana;
+
+        $total_saldo = $selisihKas + $selisihAset + $selisihDana;
+        $saldoAwal = $cashflowController->getSaldoAwal($filter, $date);
+        $saldoAkhir = $total_saldo + $saldoAwal;
+
+        return $saldoAkhir;
+    }
+
+    public function setLabaRugi($labarugiController, $filter, $date, $labarugi_group) {
+        $labarugi = $labarugiController->setResults($filter, $date, $labarugi_group);
+        $saldoLabaRugi = array_sum(array_map(function ($group) {
+            return array_sum(array_column($group, 'saldo'));
+        }, $labarugi));
+
+        return $saldoLabaRugi;
+    }
+
+    public function setTrendKeuangan($type) {
+        $currentYear = date('Y');
+        if ($type == "LastYear") {
+            $currentYear -= 1;
+        }
+
+        $jan = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-02'])->where('type', 'monthly')->sum('saldo'));
+        $feb = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-03'])->where('type', 'monthly')->sum('saldo'));
+        $mar = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-04'])->where('type', 'monthly')->sum('saldo'));
+        $apr = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-05'])->where('type', 'monthly')->sum('saldo'));
+        $may = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-06'])->where('type', 'monthly')->sum('saldo'));
+        $jun = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-07'])->where('type', 'monthly')->sum('saldo'));
+        $jul = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-08'])->where('type', 'monthly')->sum('saldo'));
+        $aug = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-09'])->where('type', 'monthly')->sum('saldo'));
+        $sep = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-10'])->where('type', 'monthly')->sum('saldo'));
+        $oct = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-11'])->where('type', 'monthly')->sum('saldo'));
+        $nov = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-12'])->where('type', 'monthly')->sum('saldo'));
+        $dec = intval(HistoryReport::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$currentYear . '-01'])->where('type', 'monthly')->sum('saldo'));
+
+        return [$jan, $feb, $mar, $apr, $may, $jun, $jul, $aug, $sep, $oct, $nov, $dec];
+
+    }
+
+
 
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -40,23 +40,6 @@
                   <!-- /.info-box -->
                 </div>
                 <!-- /.col -->
-
-                <!-- fix for small devices only -->
-                {{-- <div class="clearfix hidden-md-up"></div>
-
-                <div class="col-12 col-sm-6 col-md-3">
-                  <div class="info-box mb-3">
-                    <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
-
-                    <div class="info-box-content">
-                      <span class="info-box-text">Sales</span>
-                      <span class="info-box-number">760</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                  </div>
-                  <!-- /.info-box -->
-                </div> --}}
-                <!-- /.col -->
                 <div class="col d-flex">
                   <div class="info-box mb-3">
                     <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user-graduate"></i></span>
@@ -112,143 +95,33 @@
 
             <div class="row">
               <div class="col-lg-6">
-                {{-- <div class="card">
-                  <div class="card-header border-0">
-                    <div class="d-flex justify-content-between">
-                      <h3 class="card-title">Online Store Visitors</h3>
-                      <a href="javascript:void(0);">View Report</a>
-                    </div>
-                  </div>
-                  <div class="card-body">
-                    <div class="d-flex">
-                      <p class="d-flex flex-column">
-                        <span class="text-bold text-lg">820</span>
-                        <span>Visitors Over Time</span>
-                      </p>
-                      <p class="ml-auto d-flex flex-column text-right">
-                        <span class="text-success">
-                          <i class="fas fa-arrow-up"></i> 12.5%
-                        </span>
-                        <span class="text-muted">Since last week</span>
-                      </p>
-                    </div>
-                    <!-- /.d-flex -->
-
-                    <div class="position-relative mb-4">
-                      <canvas id="visitors-chart" height="200"></canvas>
-                    </div>
-
-                    <div class="d-flex flex-row justify-content-end">
-                      <span class="mr-2">
-                        <i class="fas fa-square text-primary"></i> This Week
-                      </span>
-                      <span>
-                        <i class="fas fa-square text-gray"></i> Last Week
-                      </span>
-                    </div>
-                  </div>
-                </div> --}}
-                <!-- /.card -->
-
                 <div class="card">
                   <div class="card-header border-0">
-                    <h3 class="card-title">Akun transaksi yang sering digunakan</h3>
-                    <div class="card-tools">
-                      <a href="#" class="btn btn-tool btn-sm">
-                        <i class="fas fa-download"></i>
-                      </a>
-                      <a href="#" class="btn btn-tool btn-sm">
-                        <i class="fas fa-bars"></i>
-                      </a>
-                    </div>
+                    <div class="d-flex justify-content-between">
+                        <h3 class="card-title">Transaksi Terbaru</h3>
+                        <a href="{{ route('transaction.index') }}">Lihat Semua Transaksi</a>
+                      </div>
                   </div>
                   <div class="card-body table-responsive p-0">
                     <table class="table table-striped table-valign-middle">
                       <thead>
                       <tr>
-                        <th>Mahasiswa</th>
-                        <th>Jenis</th>
+                        <th>Deskripsi</th>
+                        <th>Akun Transaksi</th>
+                        <th>Tipe</th>
                         <th>Nominal</th>
-                        <th>Status</th>
                       </tr>
                       </thead>
                       <tbody>
                       <tr>
-                        <td>
-                          <img src={{asset('vendor/dist/img/default-150x150.png')}} alt="Product 1" class="img-circle img-size-32 mr-2">
-                          Some Product
-                        </td>
-                        <td>$13 USD</td>
-                        <td>
-                          <small class="text-success mr-1">
-                            <i class="fas fa-arrow-up"></i>
-                            12%
-                          </small>
-                          12,000 Sold
-                        </td>
-                        <td>
-                          <a href="#" class="text-muted">
-                            <i class="fas fa-search"></i>
-                          </a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <img src={{asset('vendor/dist/img/default-150x150.png')}} alt="Product 1" class="img-circle img-size-32 mr-2">
-                          Another Product
-                        </td>
-                        <td>$29 USD</td>
-                        <td>
-                          <small class="text-warning mr-1">
-                            <i class="fas fa-arrow-down"></i>
-                            0.5%
-                          </small>
-                          123,234 Sold
-                        </td>
-                        <td>
-                          <a href="#" class="text-muted">
-                            <i class="fas fa-search"></i>
-                          </a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <img src={{asset('vendor/dist/img/default-150x150.png')}} alt="Product 1" class="img-circle img-size-32 mr-2">
-                          Amazing Product
-                        </td>
-                        <td>$1,230 USD</td>
-                        <td>
-                          <small class="text-danger mr-1">
-                            <i class="fas fa-arrow-down"></i>
-                            3%
-                          </small>
-                          198 Sold
-                        </td>
-                        <td>
-                          <a href="#" class="text-muted">
-                            <i class="fas fa-search"></i>
-                          </a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <img src={{asset('vendor/dist/img/default-150x150.png')}} alt="Product 1" class="img-circle img-size-32 mr-2">
-                          Perfect Item
-                          <span class="badge bg-danger">NEW</span>
-                        </td>
-                        <td>$199 USD</td>
-                        <td>
-                          <small class="text-success mr-1">
-                            <i class="fas fa-arrow-up"></i>
-                            63%
-                          </small>
-                          87 Sold
-                        </td>
-                        <td>
-                          <a href="#" class="text-muted">
-                            <i class="fas fa-search"></i>
-                          </a>
-                        </td>
+                        @foreach ($transactions as $row)
+                         <tr>
+                              <td>{{ $row->description }}</td>
+                              <td>{{ $row->transactionaccount->name }}</td>
+                              <td>{{ $row->type }}</td>
+                              <td>{{ 'Rp ' . number_format($row->amount, 2, ',', '.') }}</td>
+                         </tr>
+                    @endforeach
                       </tr>
                       </tbody>
                     </table>
@@ -261,23 +134,16 @@
                 <div class="card">
                     <div class="card-header">
                       <h3 class="card-title">Pembayaran UKT Mahasiswa</h3>
-      
-                      <div class="card-tools">
-                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                          <i class="fas fa-minus"></i>
-                        </button>
-                        <button type="button" class="btn btn-tool" data-card-widget="remove">
-                          <i class="fas fa-times"></i>
-                        </button>
-                      </div>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
                       <div class="row">
                         <div class="col-md-8">
+
                           <div class="chart-responsive">
                             <canvas id="pieChart" height="150"></canvas>
                           </div>
+
                           <!-- ./chart-responsive -->
                         </div>
                         <!-- /.col -->
@@ -293,7 +159,7 @@
                       <!-- /.row -->
                     </div>
                     <!-- /.card-body -->
-                    <div class="card-footer p-0">
+                    {{-- <div class="card-footer p-0">
                       <ul class="nav nav-pills flex-column">
                         <li class="nav-item">
                           <a href="#" class="nav-link">
@@ -320,62 +186,10 @@
                           </a>
                         </li>
                       </ul>
-                    </div>
+                    </div> --}}
                     <!-- /.footer -->
                   </div>
                 <!-- /.card -->
-
-                {{-- <div class="card">
-                  <div class="card-header border-0">
-                    <h3 class="card-title">Transaksi Terbaru</h3>
-                    <div class="card-tools">
-                      <a href="#" class="btn btn-sm btn-tool">
-                        <i class="fas fa-download"></i>
-                      </a>
-                      <a href="#" class="btn btn-sm btn-tool">
-                        <i class="fas fa-bars"></i>
-                      </a>
-                    </div>
-                  </div>
-                  <div class="card-body">
-                    <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
-                      <p class="text-success text-xl">
-                        <i class="ion ion-ios-refresh-empty"></i>
-                      </p>
-                      <p class="d-flex flex-column text-right">
-                        <span class="font-weight-bold">
-                          <i class="ion ion-android-arrow-up text-success"></i> 12%
-                        </span>
-                        <span class="text-muted">CONVERSION RATE</span>
-                      </p>
-                    </div>
-                    <!-- /.d-flex -->
-                    <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
-                      <p class="text-warning text-xl">
-                        <i class="ion ion-ios-cart-outline"></i>
-                      </p>
-                      <p class="d-flex flex-column text-right">
-                        <span class="font-weight-bold">
-                          <i class="ion ion-android-arrow-up text-warning"></i> 0.8%
-                        </span>
-                        <span class="text-muted">SALES RATE</span>
-                      </p>
-                    </div>
-                    <!-- /.d-flex -->
-                    <div class="d-flex justify-content-between align-items-center mb-0">
-                      <p class="text-danger text-xl">
-                        <i class="ion ion-ios-people-outline"></i>
-                      </p>
-                      <p class="d-flex flex-column text-right">
-                        <span class="font-weight-bold">
-                          <i class="ion ion-android-arrow-down text-danger"></i> 1%
-                        </span>
-                        <span class="text-muted">REGISTRATION RATE</span>
-                      </p>
-                    </div>
-                    <!-- /.d-flex -->
-                  </div>
-                </div> --}}
               </div>
               <!-- /.col-md-6 -->
             </div>
