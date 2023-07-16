2be76115620e18776594879de76dc33d4a6a9d23
Kanzul Fiqri <62295245+KanzulF@users.noreply.github.com>
2023-07-16 20:59:07 +0700

    Update 2023_05_08_094756_create_transaction_accounts_table.php
    menambahkan enum di lajurSaldo dan lajurLaporan

--- database/migrations/2023_05_08_094756_create_transaction_accounts_table.php
+++ database/migrations/2023_05_08_094756_create_transaction_accounts_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('transaction_accounts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('lajurSaldo');
-            $table->string('lajurLaporan');
+            $table->enum('lajurSaldo', ['Debit','Kredit']);
+            $table->enum('lajurLaporan', ['Laba Rugi','Neraca']);
             $table->string('description')->nullable();
             $table->decimal('kredit', 14, 2);
             $table->decimal('debit', 14, 2);
