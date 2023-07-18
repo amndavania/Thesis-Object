97ff40c2a7e1e62ac44cc86678221b0bc8d20b03
Ahmad Zidni Zainul Ikhsan <58660486+zidny-z@users.noreply.github.com>
2023-07-18 20:46:34 +0700

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="../../img/logo.webp" class="logo IAI Ibrahimy" alt="IAI Ibrahimy Logo">
-{{ $slot }}
+<img src="https://ibrahimygenteng.com/img/logo.webp" class="logo" alt="IAI Ibrahimy Logo">
 </a>
 </td>
 </tr>
